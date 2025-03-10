0f93648e245b267f75e49ca617cf04c911e5ec43
MOHAMED AFIFI <mada2010545@gmail.com>
2025-03-10 02:24:05 +0200

    dark mode

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false);
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.add("light-mode");
+        document.documentElement.classList.remove("dark-mode");
+      }
+    },
+    [isDarkMode]
+  );
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error(
+      "Used DarkModeContext outside of Provider. The context can only be used in children of the Provider"
+    );
+  return context;
+}
+
+export { useDarkMode, DarkModeProvider };
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useDarkMode } from "../context/DarkModeContext";
 // import logoLight from "../src/data/img/logo-light.png";
 
 const StyledLogo = styled.div`
@@ -11,9 +12,12 @@ const Img = styled.img`
 `;
 
 function Logo() {
+  const { isDarkMode } = useDarkMode();
+
+  const src = isDarkMode ? "./logo-dark.png" : "./logo-light.png";
   return (
     <StyledLogo>
-      <Img src="./logo-light.png" alt="Logo" />
+      <Img src={src} alt="Logo" />
     </StyledLogo>
   );
 }
